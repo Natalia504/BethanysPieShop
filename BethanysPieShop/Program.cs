9cff76466f6381af562eace960f98d5b55bee5ff
Natalia <natalia.calt@pieinsurance.com>
2022-09-02 15:45:49 -0600

    Setting up an empty app

---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,18 @@
+﻿var builder = WebApplication.CreateBuilder(args); // looks at appsetting.json and loads those settings. These are defaults! Don't change!
+
+builder.Services.AddControllersWithViews(); // Enable MVC in this app. We started with Empty app template
+
+var app = builder.Build();
+
+//add middlaware comps
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // dev exception page, only for dev env
+};
+
+app.MapDefaultControllerRoute(); // to see the pages/views
+
+app.Run();
+
